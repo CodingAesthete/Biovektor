41e7812b0ed46e73dbba4a1a61d3f812d517d2bc
Aleksandar Sultan <harry_ollI@abv.bg>
2024-11-11 08:18:08 +0200

    Update App.js and Create Pages and Routes

---
+++ src/pages/Benefits.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function Benefits() {
+  return (
+    <div>Benefits</div>
+  )
+}
---
+++ src/pages/Effects.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function Effects() {
+  return (
+    <div>Effects</div>
+  )
+}
