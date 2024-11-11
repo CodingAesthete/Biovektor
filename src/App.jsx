6f48fed6968edfebe4f56f6940160e63c409a3a5
Aleksandar Sultan <harry_ollI@abv.bg>
2024-11-11 08:45:08 +0200

    Visualize Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Benefits from './pages/Benefits';
 import Effects from './pages/Effects';
 import Method from './pages/Method';
 import Contacts from './pages/Contacts';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Benefits />} />
-        <Route path="/learn-more" element={<Effects />} />
+        <Route path="/benefits" element={<Benefits />} />
+        <Route path="/effects" element={<Effects />} />
         <Route path="/method" element={<Method />} />
         <Route path="/contacts" element={<Contacts />} />
       </Routes>
