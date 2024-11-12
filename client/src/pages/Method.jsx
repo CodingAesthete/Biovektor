a53a3c08860026c0e7f58de03e27704b2d73fd3f
Aleksandar Sultan <harry_ollI@abv.bg>
2024-11-12 15:44:30 +0200

    Resizing Method Page

--- client/src/pages/Method.jsx
+++ client/src/pages/Method.jsx
@@ -30,45 +30,40 @@ export default function Method() {
     animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.5 }}
       exit={{ opacity: 0 }}
-      className="text-lg text-justify w-5/6 mx-auto rounded-md px-10 py-6 mt-6 mb-6 bg-emerald-50"
+      className="text-lg font-changer:text-xl w-11/12 sm:w-5/6 mx-auto rounded-lg px-4 sm:px-10 py-8 mt-6 header-sm:mt-14 sm:mt-6 mb-10 bg-emerald-50 shadow-lg"
     >
-      <h1 className="text-[1.7rem] font-bold mb-6 text-center text-red-800 bg-gradient-to-r from-emerald-600 to-emerald-400 
-              text-transparent bg-clip-text drop-shadow-xl">
+      <h1 className="text-[1.7rem] font-bold mb-6 text-center text-red-800 drop-shadow-xl">
         {t('ApplicationTitle')}
       </h1>
 
       {/* Flexbox container to align images and text */}
-      <div className="flex justify-between items-center space-x-6">
-        {/* Left image */}
-        <div className="w-1/4">
-          <img
-            src={Farmer}
-            alt="Farmer working"
-            className="w-full h-auto rounded-md shadow-lg"
-          />
-        </div>
+      <div className="flex flex-col lg:flex-row justify-between items-center space-x-3 lg:space-x-6">
+  {/* Left image */}
+  <div className="hidden lg:block w-1/4">
+    <img
+      src={Farmer}
+      alt="Farmer working"
+      className="w-full h-auto rounded-md shadow-lg"
+    />
+  </div>
 
-        {/* Text content */}
-        <div className="flex-1 text-lg text-justify">
-          <p className="mb-4">
-          {t('AppP1')}</p>
-          <p className="mb-4">
-          {t('AppP2')}
-          </p>
-          <p className="mb-4">
-          {t('AppP3')}
-          </p>
-        </div>
+  {/* Text content with responsive line-height */}
+  <div className="flex-1 text-lg text-justify font-changer:text-xl font-changer:leading-extra-loose  leading-[1.4] lg:leading-[1.6] xl:leading-[1.7] 2xl:leading-extra-loose
+">
+    <p className="mb-4">{t('AppP1')}</p>
+    <p className="mb-4">{t('AppP2')}</p>
+    <p className="mb-4">{t('AppP3')}</p>
+  </div>
 
-        {/* Right image */}
-        <div className="w-1/4">
-          <img
-            src={Farmer2}
-            alt="Farmer working in the field"
-            className="w-full h-auto rounded-md shadow-lg"
-          />
-        </div>
-      </div>
+  {/* Right image */}
+  <div className="w-4/5 md:w-1/3 lg:w-1/4">
+    <img
+      src={Farmer2}
+      alt="Farmer working in the field"
+      className="w-full h-auto rounded-md shadow-lg"
+    />
+  </div>
+</div>
     </motion.div>
   );
 }
