f67739f03495cdb0af2df50a0c53078ed02c945f
Aleksandar Sultan <harry_ollI@abv.bg>
2024-11-12 16:08:23 +0200

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import userRouter from './routes/user.route.js'
+import path from 'path';
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use('/api/user', userRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.listen(3000, () => {
   console.log('Port: 3000...');
